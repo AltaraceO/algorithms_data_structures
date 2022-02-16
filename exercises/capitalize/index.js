ea20feba59723668237e32beffc6f6f8703df0c0
AltaraceO <orialtarace@gmail.com>
2022-02-16 16:50:10 +0200

    slice to capitalize

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,6 +7,32 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+//!My attempt
+function capitalize1(str) {
+  const newArr = str.split(" ");
+  const differentArr = [];
+
+  for (let word of newArr) {
+    //*slice ,if not given a second parameter, will produce the rest of the string from the position indicated -- in this case 1 - all that comes after the first letter
+    differentArr.push(`${word[0].toUpperCase()}${word.slice(1)}`);
+  }
+
+  return differentArr.join(" ");
+}
+// capitalize("a short sentence");
+
+function capitalize(str) {
+  let result = str[0].toUpperCase();
+
+  for (let i = 1; i < str.length; i++) {
+    if (str[i - 1] === " ") {
+      result += str[i].toUpperCase();
+    } else {
+      result += str[i];
+    }
+  }
+
+  return result;
+}
 
 module.exports = capitalize;
