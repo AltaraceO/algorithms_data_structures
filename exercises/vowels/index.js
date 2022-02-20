674b8533aa325e8bffe1e916da619354e99c950e
orialtarace <orialtarace@gmail.com>
2022-02-20 09:56:42 +0200

    finding vowels using 2 for of loops, includes, and RegEx with match.

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,18 +7,38 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {
+function vowels1(str) {
   let counter = 0;
   const vowels = ["a", "e", "i", "o", "u"];
 
   for (let letter of str) {
     for (let item of vowels) {
-      if (letter === item) {
-        counter++;
+      if (letter.toLowerCase() === item) {
+        counter = counter++;
       }
     }
   }
   return counter;
+}
+
+//!Solution using includes, I tried doing this but i didn't think of comparing the vowels arr to the STR instead of the other way around
+function vowels2(str) {
+  let counter = 0;
+  const theVowels = ["a", "e", "i", "o", "u"];
+
+  for (let char of str.toLowerCase()) {
+    if (theVowels.includes(char)) {
+      counter++;
+    }
+  }
+
+  return counter;
+}
+
+//!Using RegEx
+function vowels(str) {
+  const matches = str.match(/[aeiou]/gi);
+  return matches ? matches.length : 0;
 }
 
 // vowels("how are are");
