953166fae9dae1543e8548d4ab797292ffed9a49
AltaraceO <orialtarace@gmail.com>
2022-03-05 14:01:02 +0200

    Queue from Stack

--- exercises/qfroms/index.js
+++ exercises/qfroms/index.js
@@ -12,8 +12,47 @@
 //     q.remove(); // returns 1
 //     q.remove(); // returns 2
 
-const Stack = require('./stack');
+const Stack = require("./stack");
 
-class Queue {}
+class Queue {
+  constructor() {
+    //!When we create an instance of a Q it will automatically generate 2 new stacks!
+    this.firstStack = new Stack();
+    this.secondStack = new Stack();
+  }
+
+  add(record) {
+    this.firstStack.push(record);
+  }
+
+  remove() {
+    while (this.firstStack.peek()) {
+      this.secondStack.push(this.firstStack.pop());
+    }
+    //!dont immediately return because we want to restore everything back to its rightful place in stack one!
+    const toBeReturned = this.secondStack.pop();
+
+    while (this.secondStack.peek()) {
+      this.firstStack.push(this.secondStack.pop());
+    }
+
+    return toBeReturned;
+  }
+
+  peek() {
+    //!exactly the same as the remove just instead of returning POP we will return a variable that saved the PEEK!
+    while (this.firstStack.peek()) {
+      this.secondStack.push(this.firstStack.pop());
+    }
+
+    const toBeReturned = this.secondStack.peek();
+
+    while (this.secondStack.peek()) {
+      this.firstStack.push(this.secondStack.pop());
+    }
+
+    return toBeReturned;
+  }
+}
 
 module.exports = Queue;
--- exercises/stack/index.js
+++ exercises/stack/index.js
@@ -10,6 +10,23 @@
 //   s.pop(); // returns 2
 //   s.pop(); // returns 1
 
-class Stack {}
+class Stack {
+  //!constructor creating an empty array, this.ANYNAME = ... new instance of the Stack CLASS
+  constructor() {
+    this.dataName = [];
+  }
+
+  push(record) {
+    this.dataName.push(record);
+  }
+
+  pop() {
+    return this.dataName.pop();
+  }
+
+  peek() {
+    return this.dataName[this.dataName.length - 1];
+  }
+}
 
 module.exports = Stack;
